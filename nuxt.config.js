bcc83d76a2921fa08d4fee74256536325d09d3b1
NandaRusfikri <nandarusfikri@gmail.com>
2021-06-28 13:51:51 +0700

    push

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   target: 'static',
-  router: {
-    base: '/E-Vaksin/'
-  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - vaksin-online',
